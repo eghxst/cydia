02482b590efc4e08318b0fd61af64ad5d43f67b6
elower <99680726+eghxst@users.noreply.github.com>
2022-02-27 16:17:36 -0500

    Update depictions

--- depictions.js
+++ depictions.js
@@ -15,7 +15,7 @@ function extraDesc(name) {
   desc =  "<br>Includes All color schemes for the "+name+" XenHTML Widget.<br><br>Included in this pack is the following:<ul>";
   const schemes = ["GrayScale", "Maroon", "Sunflower", "Peach", "Aqua", "Lavendar", "Pastel", "Forest", "Kiddie", "Bubblegum", "Midnight", "Neon", "Vaporwave"];
   for(let i = 0; i < schemes.length; i++) {
-    desc += "<li>"+name+" "+schemes[i]+" Color Scheme</li>";
+    desc += "<li>"+name.replace("Color Pack", "")+" "+schemes[i]+" Color Scheme</li>";
   }
   desc += "</ul>";
   return desc;
